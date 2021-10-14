00fb9d4300f276c12e25a48ead9dc20938190daf
Serg <36944165+bizzquit@users.noreply.github.com>
2021-10-14 18:19:45 +0500

    ДЗ-3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// import App from './tutorial-1/App';
-import App from './tutorial-2/App';
+// import Index from './tutorial-1/Index';
+import App from './tutorial-2/App'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+)
