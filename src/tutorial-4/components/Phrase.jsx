f9e34d60acbf810e1e495cfd6e78d2dfd4bf1fa1
Serg <36944165+bizzquit@users.noreply.github.com>
2021-10-17 15:25:37 +0500

    small correct code-2

--- src/tutorial-4/components/Phrase.jsx
+++ src/tutorial-4/components/Phrase.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Phrase({ phrases = [] }) {
   return (
     <div className="list">
-      {phrases.map(({ phrase, id }) => (
+      {phrases.map(({ phrase = '', id = Number(new Date()) }) => (
         <div key={id} className="block">
           <h3>{phrase}</h3>
         </div>
