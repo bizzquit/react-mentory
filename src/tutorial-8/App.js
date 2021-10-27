d989a257b970ef882f917e0a0c5bf05bbfbefe99
Serg <36944165+bizzquit@users.noreply.github.com>
2021-10-27 23:32:31 +0500

    corrected comments

--- src/tutorial-8/App.js
+++ src/tutorial-8/App.js
@@ -2,42 +2,32 @@ import React from 'react'
 
 function App() {
   const [users, setUsers] = React.useState([])
-  const [loadingError, setLoadingError] = React.useState(false)
-
-  // const getUsers = () => {
-  //   fetch('https://6178df40aa7f3400174045b4.mockapi.io/users')
-  //     .then((res) => res.json())
-  //     .then((result) => setUsers(result))
-  // }
 
   async function getUsers() {
     try {
       let response = await fetch(
-        'https://6178df40aa7f3400174045b4.mockapi.io/users'
+        'https://6178df40aa7f3400174045b46.mockapi.io/users'
       )
-      if (response.status >= 200 && response.status < 300) {
-        setLoadingError(false)
-      }
       let users = await response.json()
       setUsers(users)
-    } catch (err) {
-      setLoadingError(true)
+    } catch (e) {
+      if (e.name === 'SyntaxError') {
+        console.log(`Что-то пошло не так - ${e.name}`)
+      } else {
+        throw e
+      }
     }
   }
 
   return (
     <div className="App">
-      {loadingError ? (
-        <h3>Произошла ошибка загрузки юзеров!</h3>
-      ) : (
-        <ul>
-          {users.map((item) => (
-            <li key={item.id}>
-              {item.firstName} {item.lastName}
-            </li>
-          ))}
-        </ul>
-      )}
+      <ul>
+        {users.map((item) => (
+          <li key={item.id}>
+            {item.firstName} {item.lastName}
+          </li>
+        ))}
+      </ul>
       <button onClick={getUsers}>Получить список юзеров</button>
     </div>
   )
